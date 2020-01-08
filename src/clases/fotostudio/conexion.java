d7897f95e0433ef865ea43d5074c5c448efca59d
UserTek <UserTek@David>
2020-01-08 16:04:20 -0500

    agrege el jbl

---
+++ src/clases/fotostudio/conexion.java
@@ -0,0 +1,17 @@
+
+package clases.fotostudio;
+
+import java.sql.*;
+public class conexion {
+    public static Connection conectar(){
+        try {
+            Connection cn=DriverManager.getConnection("jdbc:mysql://localhost/foto_studio","root","");
+            
+            return cn;
+        } catch (Exception e) {
+        }
+         return (null);
+       
+    }
+    
+}
