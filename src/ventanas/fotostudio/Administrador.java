02a664299720b7830c581edaed75caa201990794
USUARIO <USUARIO@LAPTOP-5VO18K48>
2020-01-09 07:33:09 -0500

    Proyecto para Practica Hola

--- src/ventanas/fotostudio/Administrador.java
+++ src/ventanas/fotostudio/Administrador.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ventanas.fotostudio;
 
-/**
- *
- * @author UserTek
- */
+package ventanas.fotostudio;
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+//DIEGO SANCHEZ 123456789
+
+
+
+
 public class Administrador extends javax.swing.JFrame {
 
     /**
