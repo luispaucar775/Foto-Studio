5127ddef4d896b55640b3577a790d5751e8341e4
UserTek <UserTek@David>
2020-01-08 19:57:04 -0500

    le agregue un comentario mas

--- src/ventanas/fotostudio/Tecnico.java
+++ src/ventanas/fotostudio/Tecnico.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ventanas.fotostudio;
 
+package ventanas.fotostudio;
+//probando compatibilidad david paucar
 /**
  *
  * @author UserTek
